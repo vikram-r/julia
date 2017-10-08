624805f30d44ce6e882047b204cca938c5e983b6
Vikram Ramesh <vikram132@gmail.com>
2017-10-08 01:07:18 -0700

    Buffer calculations to help reduce stuttering
    This may be a bit overkill. The main bottleneck is terminal render speed.

--- main.go
+++ main.go
@@ -1,56 +1,119 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
-	"runtime/debug"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
-	"bytes"
 )
 
-func main() {
-	// TODO temporary for debugging purposes
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println(os.Stderr, "error: ", r)
-			fmt.Printf("trace: %s", debug.Stack())
-			os.Exit(1)
-		}
-	}()
+type Config struct {
+	w, h, cX, cY, moveX, moveY float64
+	max                        int
+}
 
+func main() {
 	w, h, err := terminalDimensions()
 	if err != nil {
 		panic(err)
 	}
-	cX, cY := -0.7, 0.27015
+	cfg := Config{
+		w:     float64(w),
+		h:     float64(h),
+		cX:    -0.7,
+		cY:    0.27015,
+		max:   255,
+		moveX: 0,
+		moveY: 0,
+	}
 
-	waitTime := time.Duration(100) * time.Millisecond
+	res := make(chan result, 20)
 
-	z := 1.0
-	for {
-		result := julia(float64(w), float64(h), cX, cY, 255, z, 0, 0)
-		fmt.Print(result)
-		z += .01
-		time.Sleep(waitTime)
+	// start calculating julia sets
+	go worker(cfg, res)
+
+	// holds the output for each zoom value
+	cache := map[float64]string{}
+
+	// mutex protects the cache
+	var mutex = &sync.Mutex{}
+	// readWG ensures that reads from cache don't get starved
+	var readWG sync.WaitGroup
+
+	// start listening for results from the worker
+	go cacheWriter(cache, res, readWG, mutex)
+
+	ticker := time.NewTicker(time.Duration(150) * time.Millisecond)
+	defer ticker.Stop()
+
+	currZoom := 1.0
+	for range ticker.C {
+		// reads have priority
+		readWG.Add(1)
+		mutex.Lock()
+
+		var output string
+		output, ok := cache[currZoom]
+		delete(cache, currZoom)
+		mutex.Unlock()
+		readWG.Done()
+
+		if !ok {
+			// cache miss
+			output = julia(cfg, currZoom)
+		}
+		fmt.Print(output)
+
+		currZoom += .01
 	}
 }
 
-func julia(w float64, h float64, cX float64, cY float64, max int, zoom float64, moveX float64, moveY float64) string {
-	// TODO canvas is to improve readability, and can be removed if it affects performance
-	canvas := makeCanvas(int(w), int(h))
-	for y := 0.0; y < h; y++ {
-		for x := 0.0; x < w; x++ {
-			zx := 1.5 * (x - w/2) / (0.5 * zoom * w) + moveX
-			zy := 1.0 * (y - h/2) / (0.5 * zoom * h) + moveY
+// result is the message sent over the channel
+type result struct {
+	output string
+	zoom   float64
+}
+
+// worker performs julia set calculations, and publishes results onto a buffered channel (to prevent oom)
+func worker(cfg Config, out chan<- result) {
+	zoom := 1.0
+	for {
+		out <- result{
+			output: julia(cfg, zoom),
+			zoom:   zoom,
+		}
+		zoom += .01
+	}
+}
+
+// cacheWriter listens on the channel, and writes all results to the map
+func cacheWriter(cache map[float64]string, in <-chan result, readWG sync.WaitGroup, mutex *sync.Mutex) {
+	for r := range in {
+		readWG.Wait()
+		mutex.Lock()
+
+		cache[r.zoom] = r.output
+
+		mutex.Unlock()
+	}
+}
+
+func julia(cfg Config, zoom float64) string {
+	canvas := makeCanvas(int(cfg.w), int(cfg.h))
+	for y := 0.0; y < cfg.h; y++ {
+		for x := 0.0; x < cfg.w; x++ {
+			zx := 1.5*(x-cfg.w/2)/(0.5*zoom*cfg.w) + cfg.moveX
+			zy := 1.0*(y-cfg.h/2)/(0.5*zoom*cfg.h) + cfg.moveY
 			done := func() bool {
 				return (zx*zx + zy*zy) >= 4
 			}
-			for i := 0; i < max && !done(); i++ {
-				tmp := zx*zx - zy*zy + cX
-				zy, zx = 2.0*zx*zy+cY, tmp
+			for i := 0; i < cfg.max && !done(); i++ {
+				tmp := zx*zx - zy*zy + cfg.cX
+				zy, zx = 2.0*zx*zy+cfg.cY, tmp
 			}
 			if !done() {
 				canvas[int(y)][int(x)] = "*"
